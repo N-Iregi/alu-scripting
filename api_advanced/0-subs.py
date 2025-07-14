c64fef6ca2c2294f726611923159ee9d8183d5bb
N-Iregi <n.iregi@alustudent.com>
2025-07-14 18:06:07 +0300

    Modify 0-subs.py

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
-"""
-a function that queries the Reddit API and returns
-the number of subscribers (not active users, total subscribers)
-for a given subreddit. If an invalid subreddit is given,
-the function should return 0.
-"""
+
 import requests
 
 def number_of_subscribers(subreddit):
+    """
+    a function that queries the Reddit API and returns
+    the number of subscribers (not active users, total subscribers)
+    for a given subreddit. If an invalid subreddit is given,
+    the function should return 0.
+    """
+
     subred_url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User-Agent': 'script:0-subs.py:v1.0 by (/u/cert_iva)'}
 
